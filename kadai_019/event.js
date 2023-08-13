8e24964bea0d7be668ae4a6f60cd68539e7b222a
moooo-19 <139045588+moooo-19@users.noreply.github.com>
2023-08-13 19:47:49 +0900

    java19章課題をアップロード

---
+++ kadai_019/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const textList = document.getElementById('text-list')
+
+btn.addEventListener('click',() => {
+    const childList = document.createElement('h2');
+    text.textContent = 'ボタンをクリックしました'
+})
